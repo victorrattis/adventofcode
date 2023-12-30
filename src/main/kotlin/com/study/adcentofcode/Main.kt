70bbe630635c11170939e0b25b38d9356b06805e
Victor Rattis <victorrattis@gmail.com>
2023-12-30 19:29:55 +0000

    Complete question 08

--- src/main/kotlin/com/study/adcentofcode/Main.kt
+++ src/main/kotlin/com/study/adcentofcode/Main.kt
@@ -11,11 +11,12 @@ fun main(args: Array<String>) = runBlocking {
 //    println("Program arguments: ${args.joinToString()}")
 
 //    val result = Question05().execute("src/test/resources/question05-sample01.txt", isPart2 = true)
-    println("Processing Question 05 part 2")
+    println("Processing Question 08 part 2")
     var result = 0L
     val time = measureTime {
-        result = Question05().execute("src/test/resources/question05-sample01.txt", isPart2 = true)
+//        result = Question05().execute("src/test/resources/question05-sample01.txt", isPart2 = true)
 //        result = Question05().execute("src/test/resources/question05.txt", isPart2 = true)
+        result = Question08().execute("src/test/resources/question08.txt", part2 = true)
     }
     println("time: $time")
     println(result)
--- src/test/kotlin/com/study/adcentofcode/Question08Test.kt
+++ src/test/kotlin/com/study/adcentofcode/Question08Test.kt
@@ -1,5 +1,37 @@
 package com.study.adcentofcode
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class Question08Test
+class Question08Test {
+    @Test
+    fun checkSample01() {
+        val result = Question08().execute("src/test/resources/question08-sample01.txt")
+        assertEquals(2, result)
+    }
+
+    @Test
+    fun checkSample02() {
+        val result = Question08().execute("src/test/resources/question08-sample02.txt")
+        assertEquals(6, result)
+    }
+
+    @Test
+    fun checkPuzzle() {
+        val result = Question08().execute("src/test/resources/question08.txt")
+        assertEquals(21797, result)
+    }
+
+    @Test
+    fun checkSample01Pt2() {
+        val result = Question08().execute("src/test/resources/question08-sample03.txt", part2 = true)
+        assertEquals(6L, result)
+    }
+
+
+    @Test
+    fun checkPuzzlePt2() {
+        val result = Question08().execute("src/test/resources/question08.txt", part2 = true)
+        assertEquals(23977527174353L, result)
+    }
+}
